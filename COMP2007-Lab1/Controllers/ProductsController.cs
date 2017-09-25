1d7bc51ce9342cae2d8ab7ca3fe99ad88bcf0ca6
Mykyta <nesspire00@gmail.com>
2017-09-25 12:25:26 -0400

    First commit / uploading the lab code

---
+++ COMP2007-Lab1/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COMP2007_Lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Products/Details
+        public ActionResult Details(string Product)
+        {
+            ViewBag.Message = "You selected product: " + Product;
+            return View();
+        }
+    }
+}
---
+++ COMP2007-Lab1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="COMP2007_Lab1.MvcApplication" Language="C#" %>
